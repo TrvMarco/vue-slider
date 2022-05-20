888614691f3bd484057e0b99b96d731cc9996ad0
TrvMarco <trevisanello.marco@gmail.com>
2022-05-20 16:08:54 +0200

    aggiunto stampa thumbs

--- js/main.js
+++ js/main.js
@@ -1,27 +1,43 @@
-const images = [
-    {
-        src: 'img/01.jpg',
-        title: 'Svezia',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+const app = new Vue ({
+    el: '#root',
+    data :{
+        currentIndexImg: 0,
+        images: [
+            {
+                src: 'img/01.jpg',
+                title: 'Svezia',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            },
+            {
+                src: 'img/02.jpg',
+                title: 'Svizzera',
+                text: 'Lorem ipsum',
+            },
+            {
+                src: 'img/03.jpg',
+                title: 'Gran Bretagna',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            },
+            {
+                src: 'img/04.jpg',
+                title: 'Germania',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            },
+            {
+                src: 'img/05.jpg',
+                title: 'Paradise',
+                text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            },
+        ],
     },
-    {
-        src: 'img/02.jpg',
-        title: 'Svizzera',
-        text: 'Lorem ipsum',
-    },
-    {
-        src: 'img/03.jpg',
-        title: 'Gran Bretagna',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-    },
-    {
-        src: 'img/04.jpg',
-        title: 'Germania',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-    },
-    {
-        src: 'img/05.jpg',
-        title: 'Paradise',
-        text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-    },
-];
+    // methods:{
+    //     function prevImg(){
+    //         currentIndexImg = currentIndexImg + 1
+    //     }
+    // }
+
+})
+
+
+// TO DO:
+// - classe active da assegnare a thumb
